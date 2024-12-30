d9e8adfa22ad708298f2c60ac82e2cf5c31da64a
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2024-12-30 17:35:03 +0530

    call and this

---
+++ app.js
@@ -0,0 +1,14 @@
+function setUserName(username) {
+  this.username = username;
+  console.log("called");
+}
+
+function creatUser(username, email, password) {
+  setUserName.call(this, username);
+  /* caall function is used to reffer the object created outside the function and this is used to target the instance of the the uername */
+  this.email = email;
+  this.password = password;
+}
+
+let newUser = new creatUser("Arjun", "arjun@amazon.com", "1234");
+console.log(newUser);
